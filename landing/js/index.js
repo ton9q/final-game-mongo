ebb3bec094225218b314e9615f22598f07f6070c
Anton Kuchma <kuchma_anton@mail.ru>
2020-01-11 21:27:32 +0300

    refact: change landing styles

--- landing/js/index.js
+++ landing/js/index.js
@@ -1,29 +1,33 @@
-let slideIndex = 0;
+let currentSlideIndex = 0;
+const slides = document.getElementsByClassName('slide');
 
 function showSlides(nextSlideIndex) {
-  const slides = document.getElementsByClassName('slide');
-  const slideIndexForShow = nextSlideIndex !== 0 || nextSlideIndex > slides.length
-    ? slides.length - 1
-    : 0;
+  if (nextSlideIndex < 0) {
+    currentSlideIndex = slides.length - 1;
+  } else if (nextSlideIndex > slides.length - 1) {
+    currentSlideIndex = 0;
+  } else {
+    currentSlideIndex = nextSlideIndex;
+  }
 
   for (let i = 0; i < slides.length; i += 1) {
     slides[i].style.display = 'none';
   }
 
-  slides[slideIndexForShow].style.display = 'block';
+  slides[currentSlideIndex].style.display = 'block';
 }
 
 const buttonPrev = document.getElementById('prev-button');
 const buttonNext = document.getElementById('next-button');
 
 buttonPrev.addEventListener('click', () => {
-  slideIndex -= 1;
-  showSlides(slideIndex);
+  currentSlideIndex -= 1;
+  showSlides(currentSlideIndex);
 });
 
 buttonNext.addEventListener('click', () => {
-  slideIndex += 1;
-  showSlides(slideIndex);
+  currentSlideIndex += 1;
+  showSlides(currentSlideIndex);
 });
 
-showSlides(slideIndex);
+showSlides(currentSlideIndex);
